19b63a652b31ccf3d1ca08f52dd55a8acc110a79
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-03-24 00:58:46 +0530

    used POST method to send URL in json format

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("The URL Shortner Is Live!!!!");
 });
 
+app.post("/short", (req, res) => {
+  res.send({
+    longUrl: "https://www.google.com",
+  });
+  console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
