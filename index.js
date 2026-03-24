1e553495136f6a02e2f03622e98a7ebd46be3d0b
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-03-25 01:34:24 +0530

    feat: initialize URL shortener with POST /short route and in-memory storage
    - setup express server on port 3000
    - add POST /short route that accepts longUrl from
     request body
    - generate random alphanumeric short code using
    Math.random().toString(36)
    - store shortCode:longUrl mapping in in-memory
    urls object
    - return longUrl and shortUrl in response
    
    next: build GET /:shortCode redirect route

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+const url = {};
 app.get("/", (req, res) => {
   res.send("The URL Shortner Is Live!!!!");
 });
 
 app.post("/short", (req, res) => {
-  res.send({
-    longUrl: "https://www.google.com",
-  });
+  const short = Math.random().toString(36).substring(2, 8);
+  url[short] = req.body.longUrl;
   console.log(req.body);
+  console.log(url);
+
+  res.send({
+    longUrl: req.body.longUrl,
+    shortUrl: short,
+  });
 });
 
 app.listen(port, () => {
