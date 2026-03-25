e4e1414f49810699cb0fdfa43fd69ba4d2c46232
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-03-25 23:50:58 +0530

    learnt about express dynamic routing

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ app.get("/", (req, res) => {
 app.post("/short", (req, res) => {
   const short = Math.random().toString(36).substring(2, 8);
   url[short] = req.body.longUrl;
-  console.log(req.body);
   console.log(url);
-
   res.send({
     longUrl: req.body.longUrl,
     shortUrl: short,
   });
 });
+app.get("/url",(req, res)=>{
+  console.log(req.params.url);
+  res.redirect(url);
+})
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
