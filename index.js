cd87f466a3362c077bfe8fcf88e2a434d6f93413
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-03-28 02:15:47 +0530

    fixed the post route using dynamic routing

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-const url = {};
+const urls = {};
+
 app.get("/", (req, res) => {
   res.send("The URL Shortner Is Live!!!!");
 });
 
 app.post("/short", (req, res) => {
   const short = Math.random().toString(36).substring(2, 8);
-  url[short] = req.body.longUrl;
-  console.log(url);
+   urls[short]= req.body.longUrl ;
+  console.log(urls);
   res.send({
     longUrl: req.body.longUrl,
     shortUrl: short,
   });
 });
-app.get("/url",(req, res)=>{
-  console.log(req.params.url);
-  res.redirect(url);
+app.get("/:url",(req, res)=>{
+  const longUrl= urls[req.params.url];
+  res.redirect(longUrl);
 })
+ 
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`);
