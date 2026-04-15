3255e82a33dbc3c28298c77c83817e72fdd3f470
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-04-16 01:07:02 +0530

    stored the Urls in databse

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const Url = require("./urlModel");
+mongoose.connect("mongodb://localhost:27017/urlShortner");
 
 app.use(express.json()); //middle Ware for converting raw data to js object
 const urls = {};
@@ -10,13 +11,13 @@ app.get("/", (req, res) => {
   res.send("The URL Shortner Is Live!!!!");
 });
 
-app.post("/short", (req, res) => {
-  const short = Math.random().toString(36).substring(2, 8); //converting the long url into
-  urls[short] = req.body.longUrl;
+app.post("/short",async (req, res) => {
+   const shortUrl =await Math.random().toString(36).substring(2, 8); //converting the long url into
+  urls[shortUrl] = req.body.longUrl;
   console.log(urls);
   res.send({
     longUrl: req.body.longUrl,
-    shortUrl: short,
+    shortUrl: shortUrl,
   });
 });
 app.get("/:url", (req, res) => {
