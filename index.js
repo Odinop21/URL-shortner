36aa242ecd0aecb4c8f322905b11a74c2da26ae0
PRATUSH KUMAR GOUDA <pratush.gouda@gmail.com>
2026-04-29 00:17:15 +0530

    installed CORS module to run the app

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 const Url = require("./urlModel");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/urlShortner");
-
+app.use(cors()); //cors is used to allow the cross origin resource sharing
 app.use(express.json()); //middle Ware for converting raw data to js object
 const urls = {};
 
@@ -13,18 +14,23 @@ app.get("/", (req, res) => {
 });
 
 app.post("/short", async (req, res) => {
+  // console.log("body recived :" ,req.body);
   const shorturl = Math.random().toString(36).substring(2, 8); //converting the long url into
   const entry = new Url({ longUrl: req.body.longUrl, shortUrl: shorturl });
   await entry.save();
+  console.log("Saved:", entry);
   res.send({
     longUrl: req.body.longUrl,
     shortUrl: shorturl,
   });
 });
 app.get("/:url", async (req, res) => {
-  const urlDoc = await Url.findOne({shortUrl:req.params.url}); //shorten link redirection
+  //  console.log(req.params.url);
+  const urlDoc = await Url.findOne({ shortUrl: req.params.url }); //shorten link redirection
+  if (urlDoc == null) {
+    return res.status(404).send("The Url Is Not Found");
+  }
   res.redirect(urlDoc.longUrl);
-  console.log(req.params.url)
 });
 
 app.listen(port, () => {
